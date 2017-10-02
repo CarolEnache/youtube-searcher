86c1cddce7a826655bb2cff5789c6e63bbe520c8
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-02 15:03:05 +0100

    import and call YTsercher

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBw6M0Eg3PB63YMtUn319JKVSmoT2BSZVY';
 
+YTsearch({key: API_KEY, term: 'surfboards'}, function(data){ console.log(data);})
 
-const App = function(){
+const App = () => {
     return(
     <div>
         <SearchBar />
