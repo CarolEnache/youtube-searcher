78b30977d9c7346b019b95bdf4e62f742880780c
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-02 15:22:35 +0100

    create class based component VideoList and link it to the App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBw6M0Eg3PB63YMtUn319JKVSmoT2BSZVY';
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
