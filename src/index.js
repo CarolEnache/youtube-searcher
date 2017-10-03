692d2ebc5eed1549b588c26d79a957a5c605361d
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-03 16:06:11 +0100

    create the serch functionality

--- src/index.js
+++ src/index.js
@@ -17,19 +17,22 @@ class App extends Component {
         
         };
 
-        YTsearch({ key: API_KEY, term: 'surfboards' },(videos) => { 
-            this.setState({ 
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term){
+        YTsearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
         });
-
-    }
+     }
 
     render(){
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
