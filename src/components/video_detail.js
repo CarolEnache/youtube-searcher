b9cb544c56ba6e1dbd746556465ee1e3a48a7616
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-03 13:14:10 +0100

    build markup for the video detail

---
+++ src/components/video_detail.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const VideoDetail = (props) => {
+    return (
+        <div className='video-detail col-md-8'>
+            <div className='embed-responsive embed-responsive-16by9'>
+                <iframe className='emded-responsive-item'></iframe>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+
+        </div>
+    )
+};
+
+export default VideoDetail;
