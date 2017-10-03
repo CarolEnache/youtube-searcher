4ab7b9562752f3332450290a866b9db2208f889f
Carol Enache <carolenache@Carols-MacBook-Pro.local>
2017-10-03 13:26:31 +0100

    create the videoId and url variabes and link video_detail to the App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBw6M0Eg3PB63YMtUn319JKVSmoT2BSZVY';
 
 class App extends Component {
@@ -22,6 +23,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail />
                 <VideoList videos={this.state.videos}/>
             </div>
         );
